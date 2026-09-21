093280bf0c838141bbe9b29a442b4e3b2128a575
zowelqqee <zowel.aep@gmail.com>
2026-09-21 14:53:02 +0300

    GRUD + DIST

--- lib/transport_api/routing.ex
+++ lib/transport_api/routing.ex
@@ -3,6 +3,19 @@ defmodule TransportApi.Routing do
   alias TransportApi.Repo
   alias TransportApi.StopTime
   alias TransportApi.Stop
+
+  def nearby_stops(stop_id) do
+    stop = Repo.get!(Stop, stop_id)
+
+    lat_delta = 0.005
+    lon_delta = 0.010
+
+    from(s in Stop,
+    where:  s.feed_version_id == ^stop.feed_version_id and
+    s.lat >= ^(stop.lat - lat_delta) and s.lat <= ^(stop.lat + lat_delta) and s.lon >= ^(stop.lon - lon_delta) and s.lon <= ^(stop.lon + lon_delta))
+    |> Repo.all()
+    |> Enum.filter(fn other -> distance_m(stop, other) <= 500 end)
+  end
   def distance_m(stop1, stop2) do
     r = 6_371_000
 
@@ -12,12 +25,7 @@ defmodule TransportApi.Routing do
     dlat = lat2 - lat1
     dlon = (stop2.lon - stop1.lon) * :math.pi() / 180
 
-    a =
-      :math.sin(dlat / 2) ** 2 +
-        :math.cos(lat1) *
-          :math.cos(lat2) *
-          :math.sin(dlon / 2) ** 2
-
+    a = :math.sin(dlat / 2) ** 2 + :math.cos(lat1) * :math.cos(lat2) * :math.sin(dlon / 2) ** 2
     2 * r * :math.atan2(:math.sqrt(a), :math.sqrt(1 - a))
   end
 
@@ -44,77 +52,49 @@ defmodule TransportApi.Routing do
   end
 
   def one_transfer_routes(from_stop_id, to_stop_id, departure_sec) do
-    first =
-      from(a in StopTime,
-        join: x in StopTime,
-        on:
-          a.trip_id == x.trip_id and
-            a.stop_sequence < x.stop_sequence,
-        where:
-          a.stop_id == ^from_stop_id and
-            a.departure_sec >= ^departure_sec,
-        select: %{
+    first = from(a in StopTime,
+    join: x in StopTime,
+    on: a.trip_id == x.trip_id and a.stop_sequence < x.stop_sequence,
+    where: a.stop_id == ^from_stop_id and a.departure_sec >= ^departure_sec,
+    order_by: [asc: a.departure_sec],
+    limit: 30,
+    select: %{
           first_trip_id: a.trip_id,
-          transfer_from_stop_id: x.stop_id,
-          first_departure_sec: a.departure_sec,
-          transfer_arrival_sec: x.arrival_sec
-        }
-      )
-
-    second =
-      from(y in StopTime,
-        join: b in StopTime,
-        on:
-          y.trip_id == b.trip_id and
-            y.stop_sequence < b.stop_sequence,
-        where: b.stop_id == ^to_stop_id,
-        select: %{
-          second_trip_id: y.trip_id,
-          transfer_to_stop_id: y.stop_id,
-          transfer_departure_sec: y.departure_sec,
-          final_arrival_sec: b.arrival_sec
-        }
-      )
-
-    from(f in subquery(first),
-      join: from_stop in Stop,
-      on: from_stop.id == f.transfer_from_stop_id,
-
-      join: s in subquery(second),
-      on: f.first_trip_id != s.second_trip_id,
-
-      join: to_stop in Stop,
-      on: to_stop.id == s.transfer_to_stop_id,
-
-      where:
-        s.transfer_departure_sec >=
-          f.transfer_arrival_sec + 120,
-
-      order_by: [asc: s.final_arrival_sec],
-
-      select: %{
-        first_trip_id: f.first_trip_id,
-        second_trip_id: s.second_trip_id,
-
-        transfer_from_stop_id: f.transfer_from_stop_id,
-        transfer_to_stop_id: s.transfer_to_stop_id,
-
-        from_lat: from_stop.lat,
-        from_lon: from_stop.lon,
-        to_lat: to_stop.lat,
-        to_lon: to_stop.lon,
-
-        departure_sec: f.first_departure_sec,
-        transfer_arrival_sec: f.transfer_arrival_sec,
-        transfer_departure_sec: s.transfer_departure_sec,
-        arrival_sec: s.final_arrival_sec}
-    )
+          transfer_stop_id: x.stop_id,
+          departure_sec: a.departure_sec,
+          transfer_arrival_sec: x.arrival_sec})
     |> Repo.all()
-    |> Enum.filter(fn route ->
-      stop1 = %{lat: route.from_lat, lon: route.from_lon}
-      stop2 = %{lat: route.to_lat, lon: route.to_lon}
-      distance_m(stop1, stop2) <= 500 end)
-    |> Enum.take(3)
 
+    first
+    |> Enum.flat_map(fn first -> nearby_ids = nearby_stops(first.transfer_stop_id)|> Enum.map(fn x -> x.id end)
+      second = from(y in StopTime,
+      join: b in StopTime,
+      on: y.trip_id == b.trip_id and y.stop_sequence < b.stop_sequence,
+      where: y.stop_id in ^nearby_ids and b.stop_id == ^to_stop_id and y.departure_sec >= ^(first.transfer_arrival_sec + 120) and y.trip_id != ^first.first_trip_id,
+      select: %{
+        second_trip_id: y.trip_id,
+        transfer_to_stop_id: y.stop_id,
+        transfer_departure_sec: y.departure_sec,
+        arrival_sec: b.arrival_sec
+      })
+        |> Repo.all()
+
+      Enum.map(second, fn second ->
+        %{
+          first_trip_id: first.first_trip_id,
+          second_trip_id: second.second_trip_id,
+
+          transfer_from_stop_id: first.transfer_stop_id,
+          transfer_to_stop_id: second.transfer_to_stop_id,
+
+          departure_sec: first.departure_sec,
+          transfer_arrival_sec: first.transfer_arrival_sec,
+          transfer_departure_sec: second.transfer_departure_sec,
+          arrival_sec: second.arrival_sec
+        }
+      end)
+    end)
+    |> Enum.sort_by(fn x -> x.arrival_sec end)
+    |> Enum.take(3)
   end
 end
--- lib/transport_api_web/graphql/schema.ex
+++ lib/transport_api_web/graphql/schema.ex
@@ -6,6 +6,53 @@ defmodule TransportApiWeb.Schema do
   alias TransportApi.Repo
   alias TransportApi.Stop
 
+  input_object :route_input do
+    field :gtfs_route_id, :string
+    field :short_name, :string
+    field :long_name, :string
+    field :route_type, :integer
+    field :transport_type, :string
+    field :circular, :boolean
+    field :urban, :boolean
+    field :feed_version_id, :id
+  end
+
+  input_object :trip_input do
+    field :gtfs_trip_id, :string
+    field :direction_id, :integer
+    field :route_id, :id
+    field :service_id, :id
+    field :shape_id, :id
+    field :feed_version_id, :id
+  end
+
+  input_object :stop_input do
+    field :gtfs_stop_id, :string
+    field :name, :string
+    field :lat, :float
+    field :lon, :float
+    field :location_type, :integer
+    field :transport_type, :string
+    field :feed_version_id, :id
+  end
+
+  object :route do
+    field :id, :id
+    field :gtfs_route_id, :string
+    field :short_name, :string
+    field :long_name, :string
+    field :route_type, :integer
+    field :transport_type, :string
+    field :circular, :boolean
+    field :urban, :boolean
+    field :forward_length, :float
+    field :backward_length, :float
+    field :start_stop_name, :string
+    field :end_stop_name, :string
+    field :trip_count, :integer
+  end
+
+
   object :stop do
     field :id, :id
     field :gtfs_stop_id, :string
@@ -34,6 +81,128 @@ defmodule TransportApiWeb.Schema do
   end
 
   query do
+
+    field :routes, list_of(:route) do
+      arg :search, :string
+      arg :transport_type, :string
+      arg :sort_by, :string
+      arg :sort_order, :string
+      arg :page, :integer, default_value: 1
+      arg :page_size, :integer, default_value: 20
+
+      resolve(fn _, args, _ ->
+        page = max(args.page, 1)
+        page_size = min(max(args.page_size, 1), 100)
+
+        query = from(r in TransportApi.Route)
+
+        query =
+          if args[:search] do
+            search = "%#{args.search}%"
+            from(r in query, where: ilike(r.short_name, ^search) or ilike(r.long_name, ^search))
+          else
+            query
+          end
+
+        query =
+          if args[:transport_type] do
+            from(r in query,
+              where: r.transport_type == ^args.transport_type
+            )
+          else
+            query
+          end
+
+        query =
+          case args[:sort_by] do
+            "name" -> from(r in query, order_by: [asc: r.long_name])
+            "number" -> from(r in query, order_by: [asc: r.short_name])
+            "transport_type" -> from(r in query, order_by: [asc: r.transport_type])
+            _ -> from(r in query, order_by: [asc: r.id])
+          end
+
+        query = if args[:sort_order] == "desc" do
+            case args[:sort_by] do
+              "name" -> from(r in query, order_by: [desc: r.long_name])
+              "number" -> from(r in query, order_by: [desc: r.short_name])
+
+              "transport_type" -> from(r in query, order_by: [desc: r.transport_type])
+
+              _ -> from(r in query, order_by: [desc: r.id])
+            end
+          else
+            query
+          end
+
+        routes =
+          query
+          |> limit(^page_size)
+          |> offset(^((page - 1) * page_size))
+          |> Repo.all()
+
+        result =
+          Enum.map(routes, fn route ->
+            trips = from(t in TransportApi.Trip, where: t.route_id == ^route.id)
+            |> Repo.all()
+
+            trip_count = length(trips)
+            forward_trip = Enum.find(trips, fn trip -> trip.direction_id == 0 and not is_nil(trip.shape_id) end)
+            backward_trip = Enum.find(trips, fn trip -> trip.direction_id == 1 and not is_nil(trip.shape_id) end)
+
+            forward_length = if forward_trip do Repo.one( from sp in TransportApi.ShapePoint,
+            where: sp.shape_id == ^forward_trip.shape_id,
+            select: max(sp.dist_traveled))
+            else
+              nil
+            end
+
+            backward_length = if backward_trip do
+                Repo.one( from sp in TransportApi.ShapePoint,
+                    where: sp.shape_id == ^backward_trip.shape_id,
+                    select: max(sp.dist_traveled))
+              else
+                nil
+              end
+            first_trip = List.first(trips)
+
+            {start_name, end_name} =
+              if first_trip do
+                stop_times =
+                  from(st in TransportApi.StopTime,
+                    where: st.trip_id == ^first_trip.id,
+                    order_by: st.stop_sequence,
+                    preload: [:stop]
+                  )
+                  |> Repo.all()
+
+                case stop_times do
+                  [] -> {nil, nil}
+                  list -> {List.first(list).stop.name, List.last(list).stop.name}
+                end
+              else
+                {nil, nil}
+              end
+
+            %{
+              id: route.id,
+              gtfs_route_id: route.gtfs_route_id,
+              short_name: route.short_name,
+              long_name: route.long_name,
+              route_type: route.route_type,
+              transport_type: route.transport_type,
+              circular: route.circular,
+              urban: route.urban,
+              start_stop_name: start_name,
+              end_stop_name: end_name,
+              trip_count: trip_count,
+              forward_length: forward_length,
+              backward_length: backward_length
+            }
+          end)
+
+        {:ok, result}
+      end)
+    end
     field :stops, list_of(:stop) do
       resolve(fn _, _, _ -> {:ok, Repo.all(Stop)} end)
     end
@@ -52,5 +221,112 @@ defmodule TransportApiWeb.Schema do
         {:ok, stop_times}
       end)
     end
-end
+  end
+
+  mutation do
+    field :create_route, :route do
+    arg :input, non_null(:route_input)
+
+    resolve(fn _, %{input: input}, _ ->
+      %TransportApi.Route{}
+      |> TransportApi.Route.changeset(input)
+      |> Repo.insert()
+    end)
+  end
+
+  field :update_route, :route do
+    arg :id, non_null(:id)
+    arg :input, non_null(:route_input)
+
+    resolve(fn _, %{id: id, input: input}, _ ->
+      case Repo.get(TransportApi.Route, id) do
+        nil -> {:error, "route not found"}
+        route -> route
+          |> TransportApi.Route.changeset(input)
+          |> Repo.update()
+      end
+    end)
+  end
+
+  field :delete_route, :route do
+    arg :id, non_null(:id)
+
+    resolve(fn _, %{id: id}, _ ->
+      case Repo.get(TransportApi.Route, id) do
+        nil -> {:error, "route not found"}
+        route -> Repo.delete(route)
+      end
+    end)
+  end
+
+
+  field :create_trip, :trip do
+    arg :input, non_null(:trip_input)
+
+    resolve(fn _, %{input: input}, _ ->
+      %TransportApi.Trip{}
+      |> TransportApi.Trip.changeset(input)
+      |> Repo.insert()
+    end)
+  end
+
+  field :update_trip, :trip do
+    arg :id, non_null(:id)
+    arg :input, non_null(:trip_input)
+
+    resolve(fn _, %{id: id, input: input}, _ ->
+      case Repo.get(TransportApi.Trip, id) do
+        nil ->
+          {:error, "trip not found"}
+
+        trip ->
+          trip
+          |> TransportApi.Trip.changeset(input)
+          |> Repo.update()
+      end
+    end)
+  end
+
+  field :delete_trip, :trip do
+    arg :id, non_null(:id)
+
+    resolve(fn _, %{id: id}, _ ->
+      case Repo.get(TransportApi.Trip, id) do
+        nil -> {:error, "trip not found"}
+        trip -> Repo.delete(trip)
+      end
+    end)
+  end
+    field :create_stop, :stop do
+      arg :input, non_null(:stop_input)
+      resolve(fn _, %{input: input}, _ -> %TransportApi.Stop{}
+        |> TransportApi.Stop.changeset(input)
+        |> Repo.insert()
+      end)
+    end
+
+    field :update_stop, :stop do
+      arg :id, non_null(:id)
+      arg :input, non_null(:stop_input)
+
+      resolve(fn _, %{id: id, input: input}, _ ->
+        case Repo.get(TransportApi.Stop, id) do
+          nil -> {:error, "stop not found"}
+          stop -> stop
+            |> TransportApi.Stop.changeset(input)
+            |> Repo.update()
+        end
+      end)
+    end
+
+    field :delete_stop, :stop do
+      arg :id, non_null(:id)
+      resolve(fn _, %{id: id}, _ ->
+        case Repo.get(TransportApi.Stop, id) do
+          nil -> {:error, "stop not found"}
+          stop -> Repo.delete(stop)
+        end
+      end)
+    end
+  end
 end
