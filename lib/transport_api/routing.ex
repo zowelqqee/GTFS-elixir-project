4ebcf46e88282fc4a161137d852ace420eea9f54
zowelqqee <zowel.aep@gmail.com>
2026-09-21 14:20:51 +0300

    forgotten direct_routes back

--- lib/transport_api/routing.ex
+++ lib/transport_api/routing.ex
@@ -21,6 +21,28 @@ defmodule TransportApi.Routing do
     2 * r * :math.atan2(:math.sqrt(a), :math.sqrt(1 - a))
   end
 
+  def direct_routes(from_stop_id, to_stop_id, departure_sec) do
+    from_st = from(st in StopTime,
+    where: st.stop_id == ^from_stop_id and st.departure_sec >= ^departure_sec)
+
+    to_st = from(st in StopTime,
+    where: st.stop_id == ^to_stop_id)
+
+    from(f in subquery(from_st),
+    join: t in subquery(to_st),
+    on: f.trip_id == t.trip_id and f.stop_sequence < t.stop_sequence,
+    order_by: [asc: t.arrival_sec],
+    limit: 3,
+    select: %{
+      trip_id: f.trip_id,
+      depareture_sec: f.departure_sec,
+      arrival_sec: t.arrival_sec,
+      from_sequence: f.stop_sequence,
+      to_sequence: t.stop_sequence
+    })
+    |> Repo.all()
+  end
+
   def one_transfer_routes(from_stop_id, to_stop_id, departure_sec) do
     first =
       from(a in StopTime,
